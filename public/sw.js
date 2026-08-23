eaae01208ffa1383a9ce3ee022091442ff60d437
dlaq <dlaq@qq.com>
2026-08-24 04:50:26 +0800

    fix: register admin PWA with isolated scope

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const BUILD_HASH = '616dc65';
+const BUILD_HASH = '995191d';
 const CACHE_PREFIX = 'yinyun-admin-';
 const PRECACHE_NAME = `${CACHE_PREFIX}${BUILD_HASH}-precache`;
 const RUNTIME_CACHE_NAME = `${CACHE_PREFIX}${BUILD_HASH}-runtime`;
